f118df134c7707a15861e96a97589906503010c7
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-08-29 12:33:40 +0100

    added testing to posts

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft returns TRUE for a draft post" do
+    # binding.irb
+    assert BlogPost.new(published_at: nil).draft?
+  end
+  test "draft returns FALSE for a scheduled post" do
+    # binding.irb
+    refute BlogPost.new(published_at: 1.year.from_now).draft?
+  end
+  test "published returns TRUE for a published post" do
+    # binding.irb
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+  test "published returns FALSE for a draft post" do
+    # binding.irb
+    refute BlogPost.new(published_at: nil).published?
+  end
 end
